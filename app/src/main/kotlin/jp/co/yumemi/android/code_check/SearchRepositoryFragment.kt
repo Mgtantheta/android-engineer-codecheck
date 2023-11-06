e0cbca6624f4f28caa4342fd7010db19be10d369
Mgtantheta <freecityNJ@gmail.com>
2023-11-06 19:50:53 +0900

    fix: diff_utilをdiffUtilに変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchRepositoryFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchRepositoryFragment.kt
@@ -59,7 +59,7 @@ class SearchRepositoryFragment : Fragment(R.layout.fragment_search_repository) {
     }
 }
 
-val diff_util = object : DiffUtil.ItemCallback<item>() {
+val diffUtil = object : DiffUtil.ItemCallback<item>() {
     override fun areItemsTheSame(oldItem: item, newItem: item): Boolean {
         return oldItem.name == newItem.name
     }
@@ -72,7 +72,7 @@ val diff_util = object : DiffUtil.ItemCallback<item>() {
 
 class CustomAdapter(
     private val itemClickListener: OnItemClickListener,
-) : ListAdapter<item, CustomAdapter.ViewHolder>(diff_util) {
+) : ListAdapter<item, CustomAdapter.ViewHolder>(diffUtil) {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
