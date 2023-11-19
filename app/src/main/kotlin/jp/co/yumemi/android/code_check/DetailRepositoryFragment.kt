87bad9defa5697ae056076b0fc533bdfde79504d
Mgtantheta <freecityNJ@gmail.com>
2023-11-19 16:16:25 +0900

    fix:ViewBindingに対応

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/DetailRepositoryFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/DetailRepositoryFragment.kt
@@ -5,7 +5,9 @@ package jp.co.yumemi.android.code_check
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
@@ -16,6 +18,15 @@ class DetailRepositoryFragment : Fragment() {
     private val args: DetailRepositoryFragmentArgs by navArgs()
     private var _binding: FragmentDetailRepositoryBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailRepositoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
