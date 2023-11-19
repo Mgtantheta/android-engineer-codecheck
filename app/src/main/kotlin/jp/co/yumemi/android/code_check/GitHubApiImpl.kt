e0fb7695445d5693c48e021f2c68533111a38ba3
Mgtantheta <freecityNJ@gmail.com>
2023-11-19 21:00:51 +0900

    add: GitHubApiのインターフェースの実装を追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/GitHubApiImpl.kt
@@ -0,0 +1,22 @@
+package jp.co.yumemi.android.code_check
+
+import io.ktor.client.*
+import io.ktor.client.call.receive
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import org.json.JSONObject
+
+class GitHubApiImpl(private val client: HttpClient) : GitHubApi {
+    override suspend fun searchRepository(query: String): Result<JSONObject> {
+        return runCatching {
+            val response = client.get<HttpResponse>(Constant.BASE_URL) {
+                header("Accept", Constant.ACCEPT_HEADER)
+                parameter("q", query)
+            }
+            val jsonBody = JSONObject(response.receive<String>())
+            Result.success(jsonBody)
+        }.getOrElse {
+            Result.failure(it)
+        }
+    }
+}
