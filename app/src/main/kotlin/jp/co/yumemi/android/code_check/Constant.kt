d29ffffb37938b35f83b4265ce58ac508a149706
Mgtantheta <freecityNJ@gmail.com>
2023-11-19 20:56:53 +0900

    add: 定数を追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/Constant.kt
@@ -0,0 +1,6 @@
+package jp.co.yumemi.android.code_check
+
+object Constant {
+    const val BASE_URL = "https://api.github.com/search/repositories"
+    const val ACCEPT_HEADER = "application/vnd.github.v3+json"
+}
